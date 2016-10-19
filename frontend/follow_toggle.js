8d55a0213c316db44875d3c4e5bf6fb27fa2e7a2
App Academy Student <student@appacademy.io>
2016-10-19 14:27:58 -0700

    finish phase 1

--- frontend/follow_toggle.js
+++ frontend/follow_toggle.js
@@ -1,8 +1,72 @@
 class FollowToggle {
   constructor(el) {
     this.$el = $(el);
+    // When using JQuery::data, we have to name the attribute data-<name>
+    // and then call .data(<name>)
+    // When using JQuery::attr, we can name the attribute <name>
+    // and then call .attr(<name>)
     this.userId = this.$el.data("user-id");
     this.followState = this.$el.data("follow-state");
+    this.render();
+    this.installHandlers();
+  }
+
+  render() {
+    this.$el.data("follow-state", this.followState);
+
+    if (this.followState === "pending") {
+      this.$el.prop("disabled", true);
+    }
+    else {
+      this.$el.prop("disabled", false);
+    }
+
+    if (this.followState === "unfollowed") {
+      this.$el.html("follow");
+    } else {
+      this.$el.html("unfollow");
+    }
+  }
+
+  installHandlers() {
+    this.$el.on("click", (evt) => { this.handleClick(evt); });
+    //(this.handleClick).bind(this));
+  }
+
+  handleClick(evt) {
+    evt.preventDefault();
+    let requestType = "";
+    let newFollowState = "";
+
+    if (this.$el.data("follow-state") === "unfollowed") {
+      requestType = "POST";
+      newFollowState = "followed";
+    }
+    else {
+      requestType = "DELETE";
+      newFollowState = "unfollowed";
+    }
+
+    let oldState = this.followState;
+    this.followState = "pending";
+    this.render();
+
+    $.ajax({
+      type: requestType,
+      dataType: "json",
+      url: `/users/${parseInt($(evt.target).data("user-id"))}/follow`,
+      context: this,
+      success(data) {
+        alert("Success!");
+        this.followState = newFollowState;
+        this.render();
+      },
+      error() {
+        alert("Failure!");
+        this.followState = oldState;
+        this.render();
+      },
+    });
   }
 }
 
--- frontend/twitter.js
+++ frontend/twitter.js
@@ -1,4 +1,8 @@
-const FollowToggle = require("follow_toggle.js");
+const FollowToggle = require("./follow_toggle.js");
 $(() => {
-  
+  let followToggles = [];
+
+  $(".follow-toggle").each ((i, el) => {
+    followToggles.push(new FollowToggle(el));
+  });
 });
